bd06dc0e091ac72b7894640e648fa4d6000eb92c
Pedro Santos <p3dro.santosc@gmail.com>
2026-05-12 11:36:23 -0300

    chore: add ValidationError interface

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -4,5 +4,10 @@ export interface HttpRequest<T = any> {
 
 export interface HttpResponse<T> {
   statusCode: number;
-  body: T | string;
+  body: T | string | ValidationError[];
+}
+
+export interface ValidationError {
+  field: string;
+  message: string;
 }
