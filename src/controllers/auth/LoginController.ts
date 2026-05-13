a4724096055ca88b008580174dcd4776dd4c282b
Pedro Santos <p3dro.santosc@gmail.com>
2026-05-13 16:23:05 -0300

    feat: add LoginController

---
+++ src/controllers/auth/LoginController.ts
@@ -0,0 +1,41 @@
+import { ILoginController, ILoginUseCase, LoginResponse } from "./protocols";
+import { HttpRequest, HttpResponse, ValidationError } from "../protocols";
+import { loginSchema, LoginInput } from "../../validators/login.schema";
+import { InvalidCredentialsError } from "../../errors/auth/auth.errors";
+
+export class LoginController implements ILoginController {
+  constructor(private readonly loginUseCase: ILoginUseCase) {}
+
+  async login(
+    httpRequest: HttpRequest<LoginInput>,
+  ): Promise<HttpResponse<LoginResponse | ValidationError | string>> {
+    try {
+      const validationResult = loginSchema.parse(httpRequest.body);
+
+      const response = await this.loginUseCase.login(validationResult);
+
+      return {
+        statusCode: 200,
+        body: response,
+      };
+    } catch (error) {
+      if (error instanceof InvalidCredentialsError) {
+        return {
+          statusCode: 401,
+          body: error.message,
+        };
+      }
+      if (error instanceof Error) {
+        return {
+          statusCode: 400,
+          body: "Internal server error",
+        };
+      }
+
+      return {
+        statusCode: 400,
+        body: "Internal server error",
+      };
+    }
+  }
+}
