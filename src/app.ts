2f641a6221ec1cb405b789fe51687ca76dfe6f11
Pedro Santos <p3dro.santosc@gmail.com>
2026-05-22 21:52:14 -0300

    chore: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import dietRoutes from "./routes/diet.routes";
 import authRoutes from "./routes/auth.routes";
@@ -6,6 +7,11 @@ import authRoutes from "./routes/auth.routes";
 export const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from "./app";
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+app.listen(3333, () => {
+  console.log("Servidor rodando na porta 3333");
 });
