4537c239299d7a2f417733c1ef258cf2f54b9f21
Pedro Santos <p3dro.santosc@gmail.com>
2026-05-26 14:36:14 -0300

    feat: add routes to GetUserById flow

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,12 +4,31 @@ import { CreateUserRepository } from "../repositories/user/CreateUserRepository"
 import { CreateUserUseCase } from "../use-cases/user/CreateUserUseCase";
 import { GetUserByEmailRepository } from "../repositories/user/GetUserByEmailRepository";
 import { CreateUserController } from "../controllers/user/CreateUserController";
+import { GetUserByIdRepository } from "../repositories/user/GetUserByIdRepository";
+import { GetUserByIdUseCase } from "../use-cases/user/GetUserByIdUseCase";
+import { GetUserByIdController } from "../controllers/user/GetUserByIdController";
+import { authMiddleware } from "../middlewares/auth-middleware";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
   const users = await prisma.user.findMany();
   return res.json(users);
+});
+
+router.get("/me", authMiddleware, async (req, res) => {
+  try {
+    const getUserByIdRepository = new GetUserByIdRepository();
+    const getUserByIdUseCase = new GetUserByIdUseCase(getUserByIdRepository);
+    const getUserByIdController = new GetUserByIdController(getUserByIdUseCase);
+
+    const response = await getUserByIdController.getUserById({
+      userId: req.userId,
+    });
+    res.status(response.statusCode).json(response.body);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 router.post("/register", async (req, res) => {
