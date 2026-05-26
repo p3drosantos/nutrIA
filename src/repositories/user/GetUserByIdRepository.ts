1b782fc920810bc853fe5bc2e525dab63de5d646
Pedro Santos <p3dro.santosc@gmail.com>
2026-05-26 14:35:56 -0300

    feat: add GetUserByIdRepository

--- src/repositories/user/GetUserByIdRepository.ts
+++ src/repositories/user/GetUserByIdRepository.ts
@@ -1,12 +1,20 @@
-import { IGetUserByIdRepository } from "../../controllers/diet/protocols";
+import { IGetUserByIdRepository } from "../../controllers/user/protocols";
 import { User } from "../../models/User.model";
 import { prisma } from "../../lib/prisma";
 
 export class GetUserByIdRepository implements IGetUserByIdRepository {
-  async getUserById(id: number): Promise<User | null> {
+  async getUserById(id: number): Promise<Omit<User, "password"> | null> {
     const user = await prisma.user.findUnique({
       where: { id },
     });
-    return user;
+    if (!user) {
+      return null;
+    }
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+    };
   }
 }
