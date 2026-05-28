0551e455de1ad192805907b6136ad1fd68829e4c
Pedro Santos <p3dro.santosc@gmail.com>
2026-05-28 15:20:29 -0300

    config: allow frontend domain in cors

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://nutria-web-chi.vercel.app"],
   }),
 );
 
